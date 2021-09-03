b93b97627c3733a6c3c8e02e030303415e7a8404
LinhDuc <linhduc.nguyen6394@gmail.com>
2021-09-03 20:51:13 +0700

    test

---
+++ app/src/main/java/com/example/demobroadcastreceiver/MainActivity1.kt
@@ -0,0 +1,28 @@
+package com.example.demobroadcastreceiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val broadcast = MyBroadcastReceiver()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val filter=IntentFilter()
+
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        filter.addAction(Intent.ACTION_TIME_CHANGED)
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        registerReceiver(broadcast, filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcast)
+    }
+}
