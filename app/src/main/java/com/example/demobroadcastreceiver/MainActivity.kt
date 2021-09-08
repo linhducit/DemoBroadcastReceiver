290b485f394cd7a2ecfbff2f2674bcd2f96d1106
LinhDuc <linhduc.nguyen6394@gmail.com>
2021-09-08 12:27:22 +0700

    add a into log

--- app/src/main/java/com/example/demobroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/demobroadcastreceiver/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val filter=IntentFilter()
-        Log.e("A","Xin chao duc")
+        Log.e("A","Xin chao a duc")
         Log.e("A","Xin chao")
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
         filter.addAction(Intent.ACTION_APPLICATION_PREFERENCES)
